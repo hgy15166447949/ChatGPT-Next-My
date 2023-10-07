cfbd21320c2a64c893b5220ab4a57cdc5dbe4036
何广远 <>
2023-10-07 10:29:18 +0800

    测试

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -145,10 +145,10 @@ export function SideBar(props: { className?: string }) {
     >
       <div className={styles["sidebar-header"]} data-tauri-drag-region>
         <div className={styles["sidebar-title"]} data-tauri-drag-region>
-          ChatGPT Next
+          ChatGPT-天目
         </div>
         <div className={styles["sidebar-sub-title"]}>
-          Build your own AI assistant.
+          数之大 无所不及
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
